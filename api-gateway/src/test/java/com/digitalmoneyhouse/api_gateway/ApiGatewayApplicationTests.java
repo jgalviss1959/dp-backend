8c5a32d79a8b78ec73d945bf53f64052bfd43c71
jgalviss1959 <jgalviss1959@gmail.com>
2025-02-20 20:54:29 -0300

    estructura de archivos

--- api-gateway/src/test/java/com/digitalmoneyhouse/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/digitalmoneyhouse/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.digitalmoneyhouse.api_gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.digitalmoneyhouse.api_gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
